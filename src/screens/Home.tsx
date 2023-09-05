b65d87e88769ff85e1e8fa37c3267cd1ade1cfe0
Daniel-Cross <dan.s.cross@icloud.com>
2023-09-05 18:20:20 +0200

    added refresh control

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,12 @@
-import { useEffect } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getNewsStories } from "../store/newsDataSlice";
 import { AppDispatch, RootState } from "../store/store";
@@ -9,10 +16,17 @@ import { PALETTE } from "../utils/palette";
 
 const Home = () => {
   const { data, status } = useSelector((state: RootState) => state.news);
+  const [refreshing, setRefreshing] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(getNewsStories());
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    dispatch(getNewsStories());
+    setRefreshing(false);
   }, []);
 
   if (status === REDUX_STATUS.SUCCEEDED) {
@@ -23,13 +37,22 @@ const Home = () => {
           extraData={data}
           renderItem={({ item }) => <NewsItem {...item} />}
           keyExtractor={(item) => item.id.toString()}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         />
+      </SafeAreaView>
+    );
+  } else if (status === REDUX_STATUS.FAILED) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Failed to load</Text>
       </SafeAreaView>
     );
   } else {
     return (
       <SafeAreaView style={styles.container}>
-        <Text>Loading...</Text>
+        <ActivityIndicator size="large" color={PALETTE.dark_grey} />
       </SafeAreaView>
     );
   }
